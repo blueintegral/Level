b99ee3abf97f92556142e1624d730050b8150c78
Hunter Scott <hunter.scott@gatech.edu>
2014-09-25 23:25:30 -0500

    Implemented some more of the HAL

--- firmware/levelfirmware/HAL.c
+++ firmware/levelfirmware/HAL.c
@@ -117,12 +117,119 @@ int set_mode(char * mode){
 }
 
 
-//int get_status(void);
-//int get_device_id(void);
-//int set_message(char *buf, int length);
-//int transmit_now(void);
-//int get_message(void);
-//int set_modulation(char *mod);
+struct status get_status(void){
+
+	struct status s; //status struct we're gonna populate
+
+	s.partnum = ReadSingleReg(PARTNUM); //part number
+	s.version = ReadSingleReg(VERSION); //current version number
+	s.freqest = ReadSingleReg(FREQEST); //frequency offset estimate
+	s.lqi = ReadSingleReg(LQI); //demodulator estimate for link quality
+	s.rssi = ReadSingleReg(RSSI); // received signal strength indication
+	s.marcstate = ReadSingleReg(MARCSTATE); //control state machine state
+	s.wortime1 = ReadSingleReg(WORTIME1); //high bytes of WOR timer
+	s.wortime0 = ReadSingleReg(WORTIME0); //low byte of WOR timer
+	s.ptkstatus = ReadSingleReg(PKTSTATUS); //current GDOx status and packet status
+	s.vco_vc_dac = ReadSingleReg(VCO_VC_DAC); //current setting from PLL calibration module
+	s.txbytes = ReadSingleReg(TXBYTES); //underflow and number of bytes in the TX FIFO
+	s.rxbytes = ReadSingleReg(RXBYTES); //overflow and number of bytes in the RX FIFO
+
+	return s;
+}
+
+
+int get_device_id(void){
+
+	return ReadSingleReg(PARTNUM);
+
+}
+
+
+int transmit_now(void){
+	unsigned char txBytesLeft = PACKET_LENGTH;           // +1 for length byte
+	unsigned char packetTransmit;
+	unsigned char transmitting = 0;
+	unsigned char txPosition = 0;
+
+	//disable receiving
+	RF1AIE &= ~BIT9;                          // Disable RX interrupts
+	RF1AIFG &= ~BIT9;                         // Clear pending IFG
+	RF1AIES &= ~BIT9;                         // Switch back to to sync word
+
+	// It is possible that ReceiveOff is called while radio is receiving a packet.
+	// Therefore, it is necessary to flush the RX FIFO after issuing IDLE strobe
+	// such that the RXFIFO is empty prior to receiving a packet.
+	Strobe(RF_SIDLE);
+	Strobe(RF_SFRX);
+
+	//Set RF switches to stun. I mean, transmit.
+	P1OUT |= BIT1; //Set P1.1 high (GDO2)
+
+	//begin transmitting
+	P3OUT |= BIT6;                        // Pulse LED during Transmit
+
+	txBytesLeft = PACKET_LENGTH;
+	txPosition = 0;
+	packetTransmit = 0;
+	transmitting = 1;
+
+	Strobe( RF_STX );                         // Strobe STX
+
+	TA0CCR1   = TX_TIMER_PERIOD;              // x cycles * 1/32768 = y us
+	TA0CCTL1 |= CCIE;
+	TA0CTL |= MC_2 + TACLR;                   // put the timer in continuous mode
+
+	__bis_SR_register(LPM3_bits + GIE);
+	__no_operation();
+
+	TA0CCR1 = TX_TIMER_PERIOD;             // x cycles * 1/32768 = y us
+	TA0CCTL1 &= ~(CCIE);
+	TA0CTL &= ~(MC_3);                  // Turn off timer
+
+	P3OUT &= ~BIT6;                     // Turn off LED after Transmit
+
+	return 0;
+}
+
+int get_message(void){
+
+	unsigned char packetReceived;
+	unsigned char rxBytesLeft = PACKET_LENGTH+2; // +2 for status bytes
+	unsigned char rxPosition = 0;
+
+
+	//Enable RX
+	 RF1AIES &= ~BIT9;
+	 RF1AIFG = 0;                              // Clear pending RFIFG interrupts
+	 RF1AIE  |= BIT9;                          // Enable the sync word received interrupt
+
+	 // Radio is in IDLE following a TX, so strobe SRX to enter Receive Mode
+	 Strobe( RF_SRX );
+	 __no_operation();
+
+	//Start receiving
+	 rxBytesLeft = PACKET_LENGTH + 2;// Set maximum packet leng + 2 for appended bytes
+	 rxPosition = 0;
+	 packetReceived = 0;
+
+	 __delay_cycles(2800);                     // Wait for bytes to fill in RX FIFO
+
+	 TA0CCR1   = RX_TIMER_PERIOD;              // x cycles * 1/32768 = y us
+	 TA0CCTL1 |= CCIE;
+	 TA0CTL   |= MC_2 + TACLR;                 // Start the timer- continuous mode
+
+	 __bis_SR_register(LPM3_bits + GIE);
+	 __no_operation();
+
+	 TA0CCR1 = RX_TIMER_PERIOD;
+	 TA0CCTL1 &= ~(CCIE);
+	 TA0CTL &= ~(MC_3);                  // Turn off timer
+	 __no_operation();
+
+	return 0;
+}
+
+
 
 
 
--- firmware/levelfirmware/HAL.h
+++ firmware/levelfirmware/HAL.h
@@ -7,19 +7,36 @@
 
 #ifndef HAL_H_
 #define HAL_H_
+
+#define PACKET_LENGTH (100)
+#define  TX_TIMER_PERIOD (40)
+#define  RX_TIMER_PERIOD (85)
+
+
 //Function prototypes
 int set_datarate(float rate);
 int set_frequency(int freq);
 int set_power(int power);
 int set_mode(char * mode);
-//int get_status(void);
-//int get_device_id(void);
-//int set_message(char *buf, int length);
-//int transmit_now(void);
-//int get_message(void);
-//int set_modulation(char *mod);
+struct status get_status(void);
+int get_device_id(void);
+int transmit_now(void);
+int get_message(void);
 
-
+struct status {
+		int partnum; //part number
+		int version; //current version number
+		int freqest; //frequency offset estimate
+		int lqi; //demodulator estimate for link quality
+		int rssi; // received signal strength indication
+		int marcstate; //control state machine state
+		int wortime1; //high bytes of WOR timer
+		int wortime0; //low byte of WOR timer
+		int ptkstatus; //current GDOx status and packet status
+		int vco_vc_dac; //current setting from PLL calibration module
+		int txbytes; //underflow and number of bytes in the TX FIFO
+		int rxbytes; //overflow and number of bytes in the RX FIFO
+	};
 
 
 #endif /* HAL_H_ */
